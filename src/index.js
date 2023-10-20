0ff79c5ec2173a8d8201b657c529ba04f84f5947
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2023-10-21 04:59:21 +0530

    index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ const { log } = require('console');
 const express = require('express');
 const app = express();
 const path = require('path');
+const hbs = require('hbs');
+
+const templatePath = path.join(__dirname,'../tempelates')
+
+app.use(express.json())
+// ~ Defining that our view engine is 'HBS'
+app.set("view engine",'hbs')
+// ~ As I am using 'tempelates' instead of views so I am telling nodejs to take this name
+app.set("views",tempelatePath)
+
+app.get('/',(req,res)=>{
+    res.render('login.hbs');
+    
+})
+app.get('/signup',(req,res)=>{
+    res.render('signup.hbs');
+
+})
 
 app.listen(3000,()=>{
     console.log("Port is Connected");
